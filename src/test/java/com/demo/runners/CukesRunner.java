d9dbb40922ec15d3635b093cd67063ed5268e2bb
podshobosh <podsho1704@gmail.com>
2024-04-17 10:10:57 -0400

    changed tag to smoke

--- src/test/java/com/demo/runners/CukesRunner.java
+++ src/test/java/com/demo/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"com/demo/step_definition"},
         dryRun = false,
-        tags = "@register",
+        tags = "@smoke",
         publish = true // generates the report with public link
 
 
